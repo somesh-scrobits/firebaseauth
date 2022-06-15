10076526cc9d0a1c2b5c69db1e9c80c4673e8e40
Somesh Bhosale <58914964+somesh4062@users.noreply.github.com>
2022-06-15 21:02:10 +0530

    firebase issue

--- App.js
+++ App.js
@@ -1,14 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
 
-export default function App() {
+import React, { useState } from 'react';
+import { Button, TextInput,View, KeyboardAvoidingView } from 'react-native';
+
+import auth from '@react-native-firebase/auth';
+
+
+export default function PhoneSignIn() {
+  // If null, no SMS has been sent
+  const [confirm, setConfirm] = useState(null);
+
+  const [code, setCode] = useState('');
+
+  // Handle the button press
+  async function signInWithPhoneNumber(phoneNumber) {
+    const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
+    setConfirm(confirmation);
+  }
+
+  async function confirmCode() {
+    try {
+      await confirm.confirm(code);
+      alert("confirm")
+    } catch (error) {
+      console.log('Invalid code.');
+      alert("Invalid code")
+    }
+  }
+
+  if (!confirm) {
+    return (
+      <View style={styles.container}>
+      <Button
+      style={styles.Button}
+        title="Phone Number Sign In"
+        onPress={() => signInWithPhoneNumber('+91 814-973-1406')}
+      />
+      </View>
+      
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <KeyboardAvoidingView style={styles.container}>
+<View >
+      <TextInput style={styles.TextInput} value={code} onChangeText={text => setCode(text)} />
+      <Button title="Confirm Code" onPress={() => confirmCode()} />
     </View>
+    </KeyboardAvoidingView>
+    
   );
 }
+
+// const Stack = createNativeStackNavigator();
+
+
+// export default function App() {
+//   return (
+//     <NavigationContainer >
+//       <Stack.Navigator>
+//       <Stack.Screen name="LoginScreen" component={LoginScreen} />
+//         <Stack.Screen name="HomeScreen" component={HomeScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +72,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin:10,marginTop:10
   },
+  TextInput:{
+        backgroundColor : '#f7f7f7',
+        width: 300,
+        alignItems: "center",
+        alignSelf:"center",
+        margin: 5,
+        height: 50,
+        borderRadius: 10,
+        padding: 10
+        },
+        Button:{
+                  padding:10,
+                  marginTop: 10,
+                  alignSelf:'center',
+                  backgroundColor:'black',
+                  justifyContent:'center',
+                  borderRadius:10,
+                  
+              },
 });
